381aaf31c5e115f74621ba2bcea3bb7632b1e64e
Manni Heumann <github@lxxi.org>
2014-08-01 19:57:00 +0200

    initial commit. only stubs right now.

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Test::Skeleton::Generator - It's new $module
+
+# SYNOPSIS
+
+    use Test::Skeleton::Generator;
+
+# DESCRIPTION
+
+Test::Skeleton::Generator is ...
+
+# LICENSE
+
+Copyright (C) Manni Heumann.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Manni Heumann <github@lxxi.org>
---
+++ lib/Test/Skeleton/Generator.pm
@@ -0,0 +1,39 @@
+package Test::Skeleton::Generator;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Test::Skeleton::Generator - It's new $module
+
+=head1 SYNOPSIS
+
+    use Test::Skeleton::Generator;
+
+=head1 DESCRIPTION
+
+Test::Skeleton::Generator is ...
+
+=head1 LICENSE
+
+Copyright (C) Manni Heumann.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Manni Heumann E<lt>github@lxxi.orgE<gt>
+
+=cut
+
